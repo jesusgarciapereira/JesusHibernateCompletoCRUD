7eaa29ad0c933e714c7d165d56e99977969fc764
jgarcia <jgarcia@107-04>
2025-01-22 14:06:53 +0100

    Mucho mejor este casteo

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -127,7 +127,8 @@ public class Principal {
 							}
 
 							if (opcionSubmenuB >= 1 && opcionSubmenuB <= 5) {
-								System.out.println("Indique el saldo de la Persona (si tiene decimales, use punto \".\")");
+								System.out.println(
+										"Indique el saldo de la Persona (si tiene decimales, use punto \".\")");
 								saldo = leeDouble(sc);
 								leer(saldo, filtro);
 								opcionSubmenuB = 0;
@@ -189,195 +190,204 @@ public class Principal {
 	}
 
 	/**
-	 * Guarda un objeto en la base de datos, validando que el saldo sea mayor o igual a 0.
+	 * Guarda un objeto en la base de datos, validando que el saldo sea mayor o
+	 * igual a 0.
+	 * 
 	 * @param cosa El objeto a guardar, debe ser de tipo EntidadPersona.
 	 * @throws Exception Si ocurre un error al guardar o acceder a la base de datos.
 	 */
 	private static void guardar(Object cosa) throws Exception {
-	    instancia.abrir();
+		
+		EntidadPersona persona =  (EntidadPersona) cosa;
+	
+		instancia.abrir();
 
-	    // Verifica que el saldo sea mayor o igual a 0 antes de guardar
-	    if ((((EntidadPersona) cosa).getSaldo()) >= 0) {
-	        instancia.guardar(cosa); // Guarda el objeto en la base de datos
-	        System.out.println("----------------");
-	        System.out.println("Persona guardada");
-	        System.out.println("-----------------------------");
-	        System.out.println("idPersona: " + ((EntidadPersona) cosa).getIdPersona());
-	        System.out.println("nombre: " + ((EntidadPersona) cosa).getNombre());
-	        System.out.println("saldo: " + ((EntidadPersona) cosa).getSaldo());
-	    } else {
-	        System.out.println("Saldo incorrecto, no se ha podido guardar");
-	    }
+		// Verifica que el saldo sea mayor o igual a 0 antes de guardar
+		if (persona.getSaldo() >= 0) {
+			instancia.guardar(cosa); // Guarda el objeto en la base de datos
 
-	    instancia.cerrar(); // Cierra la conexión con la base de datos
+			System.out.println("----------------");
+			System.out.println("Persona guardada");
+			System.out.println("-----------------------------");
+			System.out.println("idPersona: " + persona.getIdPersona());
+			System.out.println("nombre: " + persona.getNombre());
+			System.out.println("saldo: " + persona.getSaldo());
+
+		} else {
+			System.out.println("Saldo incorrecto, no se ha podido guardar");
+		}
+
+		instancia.cerrar(); // Cierra la conexión con la base de datos
 	}
 
 	/**
 	 * Lee una persona de la base de datos por su ID y muestra sus detalles.
+	 * 
 	 * @param id ID de la persona a buscar.
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void leer(long id) throws Exception {
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Busca la persona por su ID
-	    EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
+		// Busca la persona por su ID
+		EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
 
-	    // Si se encuentra la persona, muestra sus detalles
-	    if (persona != null) {
-	        System.out.println("-----------------------------");
-	        System.out.println("idPersona: " + persona.getIdPersona());
-	        System.out.println("nombre: " + persona.getNombre());
-	        System.out.println("saldo: " + persona.getSaldo());
-	    } else {
-	        System.out.println("No existe ninguna Persona con el id = " + id);
-	    }
+		// Si se encuentra la persona, muestra sus detalles
+		if (persona != null) {
+			System.out.println("-----------------------------");
+			System.out.println("idPersona: " + persona.getIdPersona());
+			System.out.println("nombre: " + persona.getNombre());
+			System.out.println("saldo: " + persona.getSaldo());
+		} else {
+			System.out.println("No existe ninguna Persona con el id = " + id);
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
 
 	/**
 	 * Busca personas en la base de datos por nombre utilizando un filtro.
+	 * 
 	 * @param nombre Nombre a buscar.
 	 * @param filtro Tipo de filtro (por ejemplo, "=" o "LIKE").
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void leer(String nombre, String filtro) throws Exception {
-	    String consultaSQL;
+		String consultaSQL;
 
-	    // Construye la consulta SQL según el filtro
-	    if (filtro.equals("=")) {
-	        consultaSQL = "SELECT * FROM Personas WHERE nombre " + filtro + " '" + nombre + "'";
-	    } else {
-	        consultaSQL = "SELECT * FROM Personas WHERE nombre " + filtro + " '%" + nombre + "%'";
-	    }
+		// Construye la consulta SQL según el filtro
+		if (filtro.equals("=")) {
+			consultaSQL = "SELECT * FROM Personas WHERE nombre " + filtro + " '" + nombre + "'";
+		} else {
+			consultaSQL = "SELECT * FROM Personas WHERE nombre " + filtro + " '%" + nombre + "%'";
+		}
 
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Ejecuta la consulta y obtiene los resultados
-	    List<EntidadPersona> personas = instancia.getSesion()
-	            .createNativeQuery(consultaSQL, EntidadPersona.class)
-	            .getResultList();
+		// Ejecuta la consulta y obtiene los resultados
+		List<EntidadPersona> personas = instancia.getSesion().createNativeQuery(consultaSQL, EntidadPersona.class)
+				.getResultList();
 
-	    // Muestra los resultados si se encuentran personas
-	    if (!personas.isEmpty()) {
-	        for (EntidadPersona persona : personas) {
-	            System.out.println("-----------------------------");
-	            System.out.println("idPersona: " + persona.getIdPersona());
-	            System.out.println("nombre: " + persona.getNombre());
-	            System.out.println("saldo: " + persona.getSaldo());
-	        }
-	    } else {
-	        System.out.println("No existe ninguna Persona con el nombre " + filtro + " " + nombre);
-	    }
+		// Muestra los resultados si se encuentran personas
+		if (!personas.isEmpty()) {
+			for (EntidadPersona persona : personas) {
+				System.out.println("-----------------------------");
+				System.out.println("idPersona: " + persona.getIdPersona());
+				System.out.println("nombre: " + persona.getNombre());
+				System.out.println("saldo: " + persona.getSaldo());
+			}
+		} else {
+			System.out.println("No existe ninguna Persona con el nombre " + filtro + " " + nombre);
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
 
 	/**
 	 * Busca personas en la base de datos por saldo utilizando un filtro.
-	 * @param saldo Saldo a buscar.
+	 * 
+	 * @param saldo  Saldo a buscar.
 	 * @param filtro Tipo de filtro (por ejemplo, ">", "<", "=").
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void leer(double saldo, String filtro) throws Exception {
-	    String consultaSQL = "SELECT * FROM Personas WHERE saldo " + filtro + " " + saldo;
+		String consultaSQL = "SELECT * FROM Personas WHERE saldo " + filtro + " " + saldo;
 
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Ejecuta la consulta y obtiene los resultados
-	    List<EntidadPersona> personas = instancia.getSesion()
-	            .createNativeQuery(consultaSQL, EntidadPersona.class)
-	            .getResultList();
+		// Ejecuta la consulta y obtiene los resultados
+		List<EntidadPersona> personas = instancia.getSesion().createNativeQuery(consultaSQL, EntidadPersona.class)
+				.getResultList();
 
-	    // Muestra los resultados si se encuentran personas
-	    if (!personas.isEmpty()) {
-	        for (EntidadPersona persona : personas) {
-	            System.out.println("-----------------------------");
-	            System.out.println("idPersona: " + persona.getIdPersona());
-	            System.out.println("nombre: " + persona.getNombre());
-	            System.out.println("saldo: " + persona.getSaldo());
-	        }
-	    } else {
-	        System.out.println("No existe ninguna Persona con el saldo = " + saldo);
-	    }
+		// Muestra los resultados si se encuentran personas
+		if (!personas.isEmpty()) {
+			for (EntidadPersona persona : personas) {
+				System.out.println("-----------------------------");
+				System.out.println("idPersona: " + persona.getIdPersona());
+				System.out.println("nombre: " + persona.getNombre());
+				System.out.println("saldo: " + persona.getSaldo());
+			}
+		} else {
+			System.out.println("No existe ninguna Persona con el saldo = " + saldo);
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
 
 	/**
 	 * Actualiza los datos de una persona en la base de datos.
-	 * @param id ID de la persona a actualizar.
+	 * 
+	 * @param id     ID de la persona a actualizar.
 	 * @param nombre Nuevo nombre de la persona.
-	 * @param saldo Nuevo saldo de la persona.
+	 * @param saldo  Nuevo saldo de la persona.
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void actualizar(long id, String nombre, double saldo) throws Exception {
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Busca la persona por su ID
-	    EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
+		// Busca la persona por su ID
+		EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
 
-	    if (persona != null) {
-	        // Actualiza los datos de la persona
-	        persona.setNombre(nombre);
-	        persona.setSaldo(saldo);
-	        instancia.getSesion().update(persona); // Actualiza la persona en la base de datos
-	    } else {
-	        System.out.println("No existe ninguna Persona con el id = " + id);
-	    }
+		if (persona != null) {
+			// Actualiza los datos de la persona
+			persona.setNombre(nombre);
+			persona.setSaldo(saldo);
+			instancia.getSesion().update(persona); // Actualiza la persona en la base de datos
+		} else {
+			System.out.println("No existe ninguna Persona con el id = " + id);
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
 
 	/**
 	 * Elimina una persona de la base de datos por su ID.
+	 * 
 	 * @param id ID de la persona a eliminar.
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void borrar(long id) throws Exception {
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Busca la persona por su ID
-	    EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
+		// Busca la persona por su ID
+		EntidadPersona persona = instancia.getSesion().get(EntidadPersona.class, id);
 
-	    if (persona != null) {
-	        instancia.getSesion().delete(persona); // Elimina la persona de la base de datos
-	    } else {
-	        System.out.println("No existe ninguna Persona con el id = " + id);
-	    }
+		if (persona != null) {
+			instancia.getSesion().delete(persona); // Elimina la persona de la base de datos
+		} else {
+			System.out.println("No existe ninguna Persona con el id = " + id);
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
 
 	/**
 	 * Muestra todas las personas registradas en la base de datos.
+	 * 
 	 * @throws Exception Si ocurre un error al acceder a la base de datos.
 	 */
 	private static void muestraTodo() throws Exception {
-	    String consultaSQL = "SELECT * FROM Personas";
+		String consultaSQL = "SELECT * FROM Personas";
 
-	    instancia.abrir();
+		instancia.abrir();
 
-	    // Ejecuta la consulta y obtiene todos los registros
-	    List<EntidadPersona> personas = instancia.getSesion()
-	            .createNativeQuery(consultaSQL, EntidadPersona.class)
-	            .getResultList();
+		// Ejecuta la consulta y obtiene todos los registros
+		List<EntidadPersona> personas = instancia.getSesion().createNativeQuery(consultaSQL, EntidadPersona.class)
+				.getResultList();
 
-	    if (!personas.isEmpty()) {
-	        for (EntidadPersona persona : personas) {
-	            System.out.println("-----------------------------");
-	            System.out.println("idPersona: " + persona.getIdPersona());
-	            System.out.println("nombre: " + persona.getNombre());
-	            System.out.println("saldo: " + persona.getSaldo());
-	        }
-	    } else {
-	        System.out.println("No existe ningún elemento en la tabla Persona");
-	    }
+		if (!personas.isEmpty()) {
+			for (EntidadPersona persona : personas) {
+				System.out.println("-----------------------------");
+				System.out.println("idPersona: " + persona.getIdPersona());
+				System.out.println("nombre: " + persona.getNombre());
+				System.out.println("saldo: " + persona.getSaldo());
+			}
+		} else {
+			System.out.println("No existe ningún elemento en la tabla Persona");
+		}
 
-	    instancia.cerrar();
+		instancia.cerrar();
 	}
-
 
 	/**
 	 * Funcion que devuelve el numero entero escrito por teclado
