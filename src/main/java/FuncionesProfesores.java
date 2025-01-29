7cd39367fcd3e552d3b1835e4156e4b9bf9d3009
jgarcia <jgarcia@107-04>
2025-01-29 11:11:48 +0100

    leerPorNombre() Profesor funciona

--- src/main/java/FuncionesProfesores.java
+++ src/main/java/FuncionesProfesores.java
@@ -45,12 +45,72 @@ public class FuncionesProfesores {
 						+ profesor.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
 				System.out.println("antiguedad: " + profesor.getAntiguedad());
 			}
-		}
-		else {
+		} else {
 			System.out.println("No existe ningún Profesor");
 		}
-		
+
 		instancia.cerrar();
+	}
+
+	static void leerPorId(long idProfesor, String filtro) throws Exception {
+		String consultaSQL = "SELECT * FROM Profesores WHERE idProfesor " + filtro + " " + idProfesor;
+
+		instancia.abrir();
+
+		List<ProfesorEntity> profesores = instancia.getSesion().createNativeQuery(consultaSQL, ProfesorEntity.class)
+				.getResultList();
+
+		if (!profesores.isEmpty()) {
+			for (ProfesorEntity profesor : profesores) {
+				System.out.println("-----------------------------");
+				System.out.println("idProfesor: " + profesor.getIdProfesor());
+				System.out.println("nombre: " + profesor.getNombre());
+				System.out.println("apellidos: " + profesor.getApellidos());
+				// Para que se vea en ese formato
+				System.out.println("fechaNacimiento: "
+						+ profesor.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+				System.out.println("antiguedad: " + profesor.getAntiguedad());
+			}
+		} else {
+			System.out.println("No existe ningún Profesor con idProfesor = " + idProfesor);
+		}
+
+		instancia.cerrar();
+
+	}
+
+	static void leerPornombre(String nombre, String filtro) throws Exception {
+		String consultaSQL;
+		
+		// Construye la consulta SQL según el filtro
+		if (filtro.equals("=")) {
+			consultaSQL = "SELECT * FROM Profesores WHERE nombre " + filtro + " '" + nombre + "'";
+		} else {
+			consultaSQL = "SELECT * FROM Profesores WHERE nombre " + filtro + " '%" + nombre + "%'";
+		}
+
+		instancia.abrir();
+
+		List<ProfesorEntity> profesores = instancia.getSesion().createNativeQuery(consultaSQL, ProfesorEntity.class)
+				.getResultList();
+
+		if (!profesores.isEmpty()) {
+			for (ProfesorEntity profesor : profesores) {
+				System.out.println("-----------------------------");
+				System.out.println("idProfesor: " + profesor.getIdProfesor());
+				System.out.println("nombre: " + profesor.getNombre());
+				System.out.println("apellidos: " + profesor.getApellidos());
+				// Para que se vea en ese formato
+				System.out.println("fechaNacimiento: "
+						+ profesor.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+				System.out.println("antiguedad: " + profesor.getAntiguedad());
+			}
+		} else {
+			System.out.println("No existe ningún Profesor con el nombre " + filtro + " " + nombre);
+		}
+
+		instancia.cerrar();
+
 	}
 
 }
--- src/main/java/ProbandoProfesores.java
+++ src/main/java/ProbandoProfesores.java
@@ -4,15 +4,37 @@ import java.time.format.DateTimeFormatter;
 public class ProbandoProfesores {
 
 	public static void main(String[] args) {
+
 		
 		try {
-			FuncionesProfesores.leerTodos();
+			FuncionesProfesores.leerPornombre("ese", "LIKE");;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("No se puede mostrar");
 		} 
-		// guardar profesor
+		
+		
+		// MOSTRAR PROFESORES POR ID
+//		try {
+//			FuncionesProfesores.leerPorId(2, "<");
+//			;
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			System.out.println("No se puede mostrar");
+//		}
+
+		// MOSTRAR TODOS LOS PROFESORES
+//		try {
+//			FuncionesProfesores.leerTodos();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			System.out.println("No se puede mostrar");
+//		} 
+
+		// GUARDAR PROFESOR
 //		// RECUERDA, en el objeto debes ponerlo asi
 //		ProfesorEntity profesor = new ProfesorEntity("Jesús", "García Pereira", LocalDate.parse("1900-05-20"), 1);
 //
